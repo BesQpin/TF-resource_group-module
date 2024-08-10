473163d62d1e802a9873150c60165ca4ff7d7b56
Peter Williams <25635012+BesQpin@users.noreply.github.com>
2024-08-10 11:21:10 +0100

    added files

--- resource_group/main.tf
+++ resource_group/main.tf
@@ -1,4 +1,4 @@
-resource "azurerm_resource_group" "rg001" {
+resource "azurerm_resource_group" "rg1" {
   name      = var.name
   location  = var.location
   tags      = merge(var.default_tags, var.custom_tags)
--- resource_group/outputs.tf
+++ resource_group/outputs.tf
@@ -1,7 +1,7 @@
 output "name" {
-  value = azurerm_resource_group.rg001.name
+  value = azurerm_resource_group.rg1.name
 }
 
 output "location" {
-  value = azurerm_resource_group.rg001.location
+  value = azurerm_resource_group.rg1.location
 }
